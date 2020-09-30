608c1ed5e57f58150078e5bcf8e936e9f00b6fe0
Sandro Brayen <sandro.brayen@shopee.com>
2020-09-30 20:49:15 +0700

    Updates

---
+++ precache-manifest.82c83a62fbad7e9ef0825219ff4a5ef3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6858e419bc734e761ebbceddf5735e99",
+    "url": "/bisnis/index.html"
+  },
+  {
+    "revision": "d6bf26b761b39de6f622",
+    "url": "/bisnis/static/css/main.17a6311e.chunk.css"
+  },
+  {
+    "revision": "1f1a3e7520600cfd490e",
+    "url": "/bisnis/static/js/2.04eb93f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bisnis/static/js/2.04eb93f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6bf26b761b39de6f622",
+    "url": "/bisnis/static/js/main.51f3c7d5.chunk.js"
+  },
+  {
+    "revision": "666cc1b5277b7e0b4290",
+    "url": "/bisnis/static/js/runtime-main.89c927d8.js"
+  },
+  {
+    "revision": "44be1fb1190106825b7bbad8f8c50768",
+    "url": "/bisnis/static/media/mimpi.44be1fb1.jpg"
+  }
+]);
