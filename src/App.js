47ee277ceb53c41f32eaba578c2d6dd6d96192ad
Sandro Brayen <sandro.brayen@shopee.com>
2020-09-30 20:48:38 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import mimpi from './mimpi.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <img alt="Share4Pay" src={mimpi}/>
+    </div>
+  );
+}
+
+export default App;
